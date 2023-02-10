4fad825295c5ae7627edbaeebd58cefa205e84c6
David <david.nguyen@newpage.io>
2023-02-11 01:26:06 +0700

    add tests

--- tests/test_report_diff.py
+++ tests/test_report_diff.py
@@ -1,11 +1,8 @@
 #!/usr/bin/env python
 
 """Tests for `test_report_diff` package."""
-
 import pytest
-
 from click.testing import CliRunner
-
 from test_report_diff import cli
 
 
@@ -25,12 +22,59 @@ def test_content(response):
     # assert 'GitHub' in BeautifulSoup(response.content).title.string
 
 
-def test_command_line_interface():
-    """Test the CLI."""
+def test_help_option():
+    """Test help option"""
     runner = CliRunner()
-    result = runner.invoke(cli.main)
-    assert result.exit_code == 0
-    assert 'test_report_diff.cli.main' in result.output
     help_result = runner.invoke(cli.main, ['--help'])
     assert help_result.exit_code == 0
-    assert '--help  Show this message and exit.' in help_result.output
+    with open('data/commandline_output/help_option.txt', 'r') as f:
+        assert f.read() == help_result.output
+
+
+def test_unsupported_type():
+    """Test unsupported report type"""
+    runner = CliRunner()
+    help_result = runner.invoke(
+        cli.main,
+        ['data/cucumber/old.json', 'data/cucumber/new.json', 'test']
+    )
+    assert help_result.exit_code == 0
+    with open('data/commandline_output/unsupported_report_type.txt', 'r') as f:
+        assert f.read() == help_result.output
+
+
+def test_cucumber_report_diff():
+    """Test cucumber report diff"""
+    runner = CliRunner()
+    cucumber_data_dir = 'data/cucumber'
+    help_result = runner.invoke(
+        cli.main,
+        [
+            f'{cucumber_data_dir}/old.json',
+            f'{cucumber_data_dir}/new.json',
+            'cucumber'
+        ]
+    )
+    assert help_result.exit_code == 0
+    with open('data/commandline_output/cucumber_report_diff.txt', 'r') as f:
+        expected_report = f.read()
+    with open('data/commandline_output/cucumber_report_diff_variant_1.txt', 'r') as f:
+        assert f.read() == help_result.output or expected_report == help_result.output
+
+
+def test_allure_report_diff():
+    """Test allure report diff"""
+    from os import getcwd, path
+    runner = CliRunner()
+    allure_data_dir = path.join(getcwd(), 'data/allure')
+    help_result = runner.invoke(
+        cli.main,
+        [
+            f'{allure_data_dir}/old_report',
+            f'{allure_data_dir}/new_report',
+            'allure'
+        ]
+    )
+    assert help_result.exit_code == 0
+    with open('data/commandline_output/allure_report_diff.txt', 'r') as f:
+        assert f.read() == help_result.output
