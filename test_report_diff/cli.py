694ee3bf1e5db08ed07935409b6f3ef4eae4d139
Swaraj Ramesh <r.swaraj@newpage.io>
2023-05-16 16:44:45 +0530

    auto unzip, folder support and fails export

--- test_report_diff/cli.py
+++ test_report_diff/cli.py
@@ -1,17 +1,57 @@
 """Console script for test_report_diff."""
 import sys
 import click
+import zipfile
+import os
 
+from test_report_diff.models.status import TestStatus
+from test_report_diff.models.test_result import TestResult
 from .formatters.triage_formatter import TriageFormatter
 from .models.suite_result import TestSuiteResult
 from .processors.cuke_json import CucumberJsonProcessor
 from .models.diff import TestResultDiff
 
 
+def unzip_file(file_path: str, destination: str = None):
+    """Unzip the file if it is a zip file"""
+    click.echo(f"Unzipping '{file_path}'")
+    with zipfile.ZipFile(file_path, 'r') as zip_ref:
+        zip_ref.extractall(destination)
+
+
+def unzip_if_needed(dir_path):
+    """if the path is a directory and it contains only zip files, then extract them"""
+    if not os.path.isdir(dir_path):
+        return
+
+    files = os.listdir(dir_path)
+    zip_files = [file for file in files if file.endswith('.zip')]
+    if len(files) == len(zip_files):
+        for file in files:
+            unzip_file(os.path.join(dir_path, file), dir_path)
+
+
+def export_fails_to_csv(file_name: str, results_by_status: dict[TestStatus, list[TestResult]]):
+    count: int = 0
+    with open(file_name, 'w') as csv_file:
+        csv_file.write('Status,Feature,Scenario,Error\n')
+        for status in [TestStatus.FAIL, TestStatus.SKIP, TestStatus.ERROR, TestStatus.UNKNOWN]:
+            count += len(results_by_status[status])
+            for test in results_by_status[status]:
+                csv_file.write(f"{test.status.value},{test.feature_or_class_name},{test.scenario_name},{test.error_message}\n")
+
+    click.echo(f"Exported {count} tests to '{file_name}'")
+
+
 @click.command()
-@click.argument('old_report_path', required=True, type=click.Path(exists=True, dir_okay=False))
-@click.argument('new_report_path', required=True, type=click.Path(exists=True, dir_okay=False))
-def main(old_report_path: str, new_report_path: str):
+@click.argument('old_report_path', required=True, type=click.Path(exists=True, dir_okay=True))
+@click.argument('new_report_path', required=True, type=click.Path(exists=True, dir_okay=True))
+@click.option('-eo', '--export_old_fails', default='', type=click.Path(dir_okay=False))
+@click.option('-en', '--export_new_fails', default='', type=click.Path(dir_okay=False))
+def main(old_report_path: str, new_report_path: str, export_old_fails: str, export_new_fails: str):
+    unzip_if_needed(old_report_path)
+    unzip_if_needed(new_report_path)
+
     orig_results: TestSuiteResult = CucumberJsonProcessor(old_report_path).get_as_test_suite_result()
     new_results: TestSuiteResult = CucumberJsonProcessor(new_report_path).get_as_test_suite_result()
 
@@ -19,6 +59,12 @@ def main(old_report_path: str, new_report_path: str):
     diff.calculate_diff()
 
     click.echo(TriageFormatter(diff).format())
+
+    if export_old_fails:
+        export_fails_to_csv(export_old_fails, diff.old_tests_by_status)
+
+    if export_new_fails:
+        export_fails_to_csv(export_new_fails, diff.new_tests_by_status)
 
     return 0
 
--- test_report_diff/processors/cuke_json.py
+++ test_report_diff/processors/cuke_json.py
@@ -1,6 +1,7 @@
 import json
 import logging
 from typing import Any, Set
+from pathlib import Path
 
 from ..models.status import TestStatus
 from ..models.suite_result import TestSuiteResult
@@ -10,10 +11,25 @@ logger = logging.getLogger(__name__)
 
 
 class CucumberJsonProcessor:
-    def __init__(self, file_path):
-        self.file_path = file_path
-        with open(file_path, 'r') as f:
-            self.json_data = json.load(f)
+
+    def __init__(self, file_path: str = None):
+        self.file_paths: list[str] = []
+        self.json_array = []
+        if file_path:
+            self.import_from(file_path)
+
+    def __import_file(self, path):
+        self.file_paths.append(path)
+        logger.debug(f"Parsing '{path}'")
+        with open(path, 'r') as f:
+            self.json_array.append(json.load(f))
+
+    def import_from(self, path: str):
+        if Path(path).is_dir():
+            for file in Path(path).glob('*.json'):
+                self.__import_file(file)
+        else:
+            self.__import_file(path)
 
     @staticmethod
     def __get_from_json(obj, key: str, default=None) -> Any:
@@ -26,88 +42,89 @@ class CucumberJsonProcessor:
     def get_as_test_suite_result(self) -> TestSuiteResult:
         suite = TestSuiteResult()
 
-        logger.debug(f"Parsing '{self.file_path}'")
-        # for each feature in the json file
-        for feature in self.json_data:
-            # if the object is not a feature, skip it
-            if not self.__get_from_json(feature, 'keyword') == 'Feature':
-                continue
-
-            # capture details of the feature
-            feature_name = self.__get_from_json(feature, 'name')
-            logger.debug(f"Found feature '{feature_name}'")
-            feature_path = self.__get_from_json(feature, 'uri')
-            feature_id = self.__get_from_json(feature, 'id')
-            logger.debug(f"Feature ID: {feature_id}")
-
-            tags_json = self.__get_from_json(feature, 'tags', [])
-            feature_tags: Set = {tag['name'] for tag in tags_json}
-
-            # for each scenario in the feature
-            for scenario in self.__get_from_json(feature, 'elements', default=[]):
-                # if the object is not a scenario, skip
-                if not self.__get_from_json(scenario, 'keyword') == 'Scenario':
+        # for the data from each json file in the json array
+        for json_data in self.json_array:
+            # for each feature in the json file
+            for feature in json_data:
+                # if the object is not a feature, skip it
+                if not self.__get_from_json(feature, 'keyword') == 'Feature':
                     continue
 
-                # capture details of the scenario
-                test_result: TestResult = TestResult()
+                # capture details of the feature
+                feature_name = self.__get_from_json(feature, 'name')
+                logger.debug(f"Found feature '{feature_name}'")
+                feature_path = self.__get_from_json(feature, 'uri')
+                feature_id = self.__get_from_json(feature, 'id')
+                logger.debug(f"Feature ID: {feature_id}")
 
-                test_result.feature_or_class_name = feature_name
-                test_result.feature_or_test_file = feature_path if feature_path else feature_id
-                test_result.scenario_id = self.__get_from_json(scenario, 'id')
-                test_result.scenario_name = self.__get_from_json(scenario, 'name')
-                logger.debug(f"Found scenario - '{test_result.scenario_name}'")
-                logger.debug(f"Scenario ID - '{test_result.scenario_id}'")
+                tags_json = self.__get_from_json(feature, 'tags', [])
+                feature_tags: Set = {tag['name'] for tag in tags_json}
 
-                scenario_tags: Set = {tag['name'] for tag in self.__get_from_json(scenario, 'tags', [])}
-                test_result.tags = list(feature_tags | scenario_tags)
+                # for each scenario in the feature
+                for scenario in self.__get_from_json(feature, 'elements', default=[]):
+                    # if the object is not a scenario, skip
+                    if not self.__get_from_json(scenario, 'keyword') == 'Scenario':
+                        continue
 
-                # by default, assume that the test has passed. Will be updated later if the test did not pass
-                test_result.status = TestStatus.PASS
+                    # capture details of the scenario
+                    test_result: TestResult = TestResult()
 
-                last_passed_step: str = ''
-                last_passed_step_line_num: int = 0
-                scenario_duration: float = 0
+                    test_result.feature_or_class_name = feature_name
+                    test_result.feature_or_test_file = feature_path if feature_path else feature_id
+                    test_result.scenario_id = self.__get_from_json(scenario, 'id')
+                    test_result.scenario_name = self.__get_from_json(scenario, 'name')
+                    logger.debug(f"Found scenario - '{test_result.scenario_name}'")
+                    logger.debug(f"Scenario ID - '{test_result.scenario_id}'")
 
-                # for each step in the scenario
-                for step in self.__get_from_json(scenario, 'steps', default=[]):
-                    result = self.__get_from_json(step, 'result', default={})
+                    scenario_tags: Set = {tag['name'] for tag in self.__get_from_json(scenario, 'tags', [])}
+                    test_result.tags = list(feature_tags | scenario_tags)
 
-                    status_str: str = self.__get_from_json(result, 'status', default='')
-                    logger.debug(f"Actual step status: {status_str}")
-                    status: TestStatus = TestStatus.from_string(status_str)
-                    logger.debug(f"Identified step status: {status}")
+                    # by default, assume that the test has passed. Will be updated later if the test did not pass
+                    test_result.status = TestStatus.PASS
 
-                    step_text: str = self.__get_from_json(step, 'keyword', default='') + \
-                                        self.__get_from_json(step, 'name', default='')
-                    step_line_num: int = self.__get_from_json(step, 'line', default=0)
+                    last_passed_step: str = ''
+                    last_passed_step_line_num: int = 0
+                    scenario_duration: float = 0
 
-                    step_duration: int = self.__get_from_json(result, 'duration', default=0)
-                    scenario_duration += (step_duration/1000000000)  # convert nanoseconds to seconds
+                    # for each step in the scenario
+                    for step in self.__get_from_json(scenario, 'steps', default=[]):
+                        result = self.__get_from_json(step, 'result', default={})
 
-                    # if the step had passed, capture the step info and move to the next step
-                    if status == TestStatus.PASS:
-                        last_passed_step = step_text
-                        last_passed_step_line_num = step_line_num
-                    else:
-                        # if the step has any status other than pass, capture the step info and break
-                        test_result.status = status
+                        status_str: str = self.__get_from_json(result, 'status', default='')
+                        logger.debug(f"Actual step status: {status_str}")
+                        status: TestStatus = TestStatus.from_string(status_str)
+                        logger.debug(f"Identified step status: {status}")
 
-                        test_result.failed_step = step_text
-                        test_result.failed_step_line_num = step_line_num
+                        step_text: str = self.__get_from_json(step, 'keyword', default='') + \
+                                            self.__get_from_json(step, 'name', default='')
+                        step_line_num: int = self.__get_from_json(step, 'line', default=0)
 
-                        error_trace: str = self.__get_from_json(result, 'error_message', default=None)
-                        test_result.error_trace = error_trace
-                        test_result.error_message = error_trace.splitlines()[0] if error_trace else None
+                        step_duration: int = self.__get_from_json(result, 'duration', default=0)
+                        scenario_duration += (step_duration/1000000000)  # convert nanoseconds to seconds
 
-                        test_result.last_passed_step = last_passed_step
-                        test_result.last_passed_step_line_num = last_passed_step_line_num
+                        # if the step had passed, capture the step info and move to the next step
+                        if status == TestStatus.PASS:
+                            last_passed_step = step_text
+                            last_passed_step_line_num = step_line_num
+                        else:
+                            # if the step has any status other than pass, capture the step info and break
+                            test_result.status = status
 
-                        break
+                            test_result.failed_step = step_text
+                            test_result.failed_step_line_num = step_line_num
 
-                test_result.duration_in_seconds = scenario_duration
-                logger.debug(f"Identified scenario status: {test_result.status}")
-                logger.debug(f"Scenario duration: {scenario_duration}")
-                suite.add_test_result(test_result)
+                            error_trace: str = self.__get_from_json(result, 'error_message', default=None)
+                            test_result.error_trace = error_trace
+                            test_result.error_message = error_trace.splitlines()[0] if error_trace else None
+
+                            test_result.last_passed_step = last_passed_step
+                            test_result.last_passed_step_line_num = last_passed_step_line_num
+
+                            break
+
+                    test_result.duration_in_seconds = scenario_duration
+                    logger.debug(f"Identified scenario status: {test_result.status}")
+                    logger.debug(f"Scenario duration: {scenario_duration}")
+                    suite.add_test_result(test_result)
 
         return suite
